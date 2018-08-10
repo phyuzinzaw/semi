de61d5f0dd3b2be1baf123499fe2102d01af607e
phyuzinzaw <phyuzinzaw644@gmail.com>
2018-08-11 01:06:03 +0630

    add

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('Home.html')
+
+@app.route('/author')
+def author():
+    return render_template('Author.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
